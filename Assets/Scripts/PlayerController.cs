1074db349e8cbf9cfef9959b23aa67ef0c15bb89
Magnus Cochems <magnus@cochems.net>
2024-08-27 04:28:00 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : NetworkBehaviour {
+    [SerializeField] private float _movementSpeed = 1f;
+    [SerializeField] private float _mouseSensitivity = 1f;
+    [SerializeField] private Transform _body;
+    private Rigidbody _rigidbody;
+    private Camera _camera;
+
+    private void Awake() {
+        _rigidbody = GetComponent<Rigidbody>();
+        _camera = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update() {
+        if (!isLocalPlayer) return;
+        
+        _rigidbody.velocity = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0) * (new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * _movementSpeed);
+        transform.RotateAround(_body.position, Vector3.up, Input.GetAxis("Mouse X") * _mouseSensitivity);
+        _camera.transform.eulerAngles -= new Vector3(Input.GetAxis("Mouse Y") * _mouseSensitivity, 0, 0);
+    }
+}
